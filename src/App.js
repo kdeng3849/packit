d9d8742e9593d2a49540f25a57dc4ee70f187dbd
Kelly Deng <kelly.deng@stonybrook.edu>
2019-03-16 15:17:33 -0400

    Basic layout, devices list view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/headers/Header'
+import DevicesList from './components/devices/DevicesList'
 
 class App extends Component {
+  state = {
+    devices: []
+  }
+
+  componentDidMount() {
+    fetch('https://api.packet.net/projects/87d7d210-e5f3-449e-a6e5-babd25af58bf/devices', {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "X-Auth-Token": "hD7cM283nx3sNmdYWDKfBpPWwcYyFUYw",
+          "Content-Type": "application/json",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(response => {
+        console.log(response.devices)
+        this.setState({ devices: response.devices })
+    });
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <div className="mx-5 my-3 p-3 bg-white rounded shadow-sm text-left">
+                <h6 className="border-bottom border-gray px-3 pb-2 mb-0">Devices</h6>
+                  <DevicesList devices={this.state.devices} />
+                <small className="d-block text-right mt-3">
+                <a href="#">All suggestions</a>
+                </small>
+            </div>
       </div>
     );
   }
